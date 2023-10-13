7c5c1084e3a9af3988c7483e0b3023a9a2b1db54
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2023-10-13 15:27:13 +0100

    updated the origin url in the socket.io server initialization

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,14 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const conversationRoutes = require("./routes/conversation");
 
-// initialize app and socket.io
-const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173"] },
-});
-
-app.use(cors());
-
 // middleware functions
 app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+
+// initialize app and socket.io
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
 
 // routes
 app.use("/user", userRoutes);
